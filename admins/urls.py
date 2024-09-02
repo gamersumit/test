7c58f9f09fda0de169067b7107b76b30175de3b1
priyanshukatiyar1404 <133838758+priyanshukatiyar1404@users.noreply.github.com>
2024-09-02 15:54:13 +0530

    Update urls.py

--- admins/urls.py
+++ admins/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('signup/', AdminCreateView.as_view(), name='register'),
     path('signin/', AdminLoginView.as_view(), name='login'),
+    path('google/oauth/', AdminGoogleOauthView.as_view(), name='google'),
 ]
