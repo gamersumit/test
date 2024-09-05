abe3bf8da76529c055009ca509bec20c7793a68d
priyanshukatiyar1404 <133838758+priyanshukatiyar1404@users.noreply.github.com>
2024-09-05 12:56:36 +0530

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
   'AUTH_HEADER_TYPES': ('Bearer',),
-  'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+  'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
   'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
   'BLACKLIST_AFTER_ROTATION': True,
   'ROTATE_REFRESH_TOKENS': True,
