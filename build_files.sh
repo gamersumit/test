5e49cffedde2386873390aa9d2f7e07a3299c93a
priyanshukatiyar1404 <133838758+priyanshukatiyar1404@users.noreply.github.com>
2024-09-16 13:09:37 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,13 @@
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py migrate
- python3 manage.py collectstatic 
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Run Django collectstatic to gather static files
+python3 manage.py collectstatic --noinput
