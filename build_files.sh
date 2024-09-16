60cab0c5e404fe454cb23b85c51f6619abf7ac17
priyanshukatiyar1404 <133838758+priyanshukatiyar1404@users.noreply.github.com>
2024-09-16 12:46:33 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
  echo "BUILD START"
+ python3 manage.py collectstatic --noinput
  python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py migrate
  echo "BUILD END"
--- tracker/settings.py
+++ tracker/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
