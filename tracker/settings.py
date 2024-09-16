7fe9b2439d5c0be11ee5d89daca123880c1992e4
priyanshukatiyar1404 <133838758+priyanshukatiyar1404@users.noreply.github.com>
2024-09-16 12:59:03 +0530

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
