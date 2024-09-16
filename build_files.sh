7a494cd975e22fbf308dfb646806098b7dd46c77
priyanshukatiyar1404 <133838758+priyanshukatiyar1404@users.noreply.github.com>
2024-09-16 13:05:21 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
  echo "BUILD START"
- python3 manage.py collectstatic --noinput
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py migrate
+ python3 manage.py collectstatic --noinput
  echo "BUILD END"
